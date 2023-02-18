5a105f2a4be43c769be4cf1012a03ebadf7ebdcc
Stephen Moore <judo2000@comcast.net>
2023-02-18 08:08:16 -0700

    Create activate account functionality in backend

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const {
   validateEmail,
   validateLength,
@@ -89,5 +90,21 @@ exports.register = async (req, res) => {
     //res.json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+};
+
+exports.activateAccount = async (req, res) => {
+  const { token } = req.body;
+  const user = jwt.verify(token, process.env.TOKEN_SECRET);
+  const check = await User.findById(user.id);
+  if (check.verified === true) {
+    return res
+      .status(400)
+      .json({ message: 'This account is alreay activated.' });
+  } else {
+    await User.findByIdAndUpdate(user.id, { verified: true });
+    res
+      .status(200)
+      .json({ message: 'Account has been activated successfully.' });
   }
 };
