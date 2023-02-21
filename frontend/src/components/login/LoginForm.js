4ee635008eb4ec1290f88320d2011cbb4c5e0098
Stephen Moore <judo2000@comcast.net>
2023-02-21 15:09:49 -0700

    Move the login form and login footer to ther own components

---
+++ frontend/src/components/login/LoginForm.js
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import { Link } from 'react-router-dom';
+import LoginInput from '../inputs/loginInput';
+
+const loginInfo = {
+  email: '',
+  password: '',
+};
+const LoginForm = () => {
+  const [login, setLogin] = useState(loginInfo);
+  const { email, password } = login;
+
+  const handleLoginChange = (e) => {
+    const { name, value } = e.target;
+    setLogin({ ...login, [name]: value });
+  };
+
+  const loginValidation = Yup.object({
+    email: Yup.string()
+      .required('Email address is required.')
+      .email('Must be a valid email.')
+      .max(100),
+    password: Yup.string().required('Password is required'),
+  });
+  return (
+    <div className="login_wrap">
+      <div className="login_1">
+        <img src="../../icons/facebook.svg" alt="" />
+        <span>
+          Facebook helps you connect and share with the people in your life.
+        </span>
+      </div>
+      <div className="login_2">
+        <div className="login_2_wrap">
+          <Formik
+            enableReinitialize
+            initialValues={{
+              email,
+              password,
+            }}
+            validationSchema={loginValidation}
+          >
+            {(formik) => (
+              <Form>
+                <LoginInput
+                  type="text"
+                  name="email"
+                  placeholder="Email address or Phone number"
+                  onChange={handleLoginChange}
+                />
+                <LoginInput
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={handleLoginChange}
+                  bottom
+                />
+                <button type="submit" className="blue_btn">
+                  Log In
+                </button>
+              </Form>
+            )}
+          </Formik>
+          <Link to="/forgot" className="forgot_password">
+            Forget password?
+          </Link>
+          <div className="sign_splitter"></div>
+          <button className="blue_btn open_signup">Create Account</button>
+        </div>
+        <Link to="/" className="sign_extra">
+          <b>Create a Page</b> for a celebrity, band or business.
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default LoginForm;
