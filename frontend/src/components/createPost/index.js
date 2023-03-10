c28032b4df68e285a4863fbc2055695af0826341
Stephen Moore <judo2000@comcast.net>
2023-03-10 07:49:35 -0700

    Build create post component

---
+++ frontend/src/components/createPost/index.js
@@ -0,0 +1,32 @@
+import { Feeling, LiveVideo, Photo } from '../../svg';
+import './style.css';
+
+const CreatePost = ({ user }) => {
+  return (
+    <div className="createPost">
+      <div className="createPost_header">
+        <img src={user?.picture} alt="" />
+        <div className="open_post hover2">
+          What's on your mind, {user?.first_name}?
+        </div>
+      </div>
+      <div className="splitter"></div>
+      <div className="createPost_body">
+        <div className="createPost_icon hover1">
+          <LiveVideo color="#f3525f" />
+          Live Video
+        </div>
+        <div className="createPost_icon hover1">
+          <Photo color="#4bbf67" />
+          Photo/Video
+        </div>
+        <div className="createPost_icon hover1">
+          <Feeling color="#f7b928" />
+          Feeling/activity
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatePost;
