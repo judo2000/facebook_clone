4ac19de2f3d89684c29fdb59697a005d8c95d1a4
Stephen Moore <judo2000@comcast.net>
2023-03-12 09:26:41 -0600

    Finish updateing home page styles for responsiveness

--- frontend/src/components/home/stories/index.js
+++ frontend/src/components/home/stories/index.js
@@ -11,8 +11,22 @@ const Stories = () => {
   const query1030px = useMediaQuery({
     query: '(max-width: 1030px)',
   });
+  const query960px = useMediaQuery({
+    query: '(max-width: 960px)',
+  });
+  const query890px = useMediaQuery({
+    query: '(max-width: 890px)',
+  });
 
-  const max = query1030px ? 5 : query1175px ? 4 : stories.length;
+  const max = query890px
+    ? 5
+    : query960px
+    ? 4
+    : query1030px
+    ? 5
+    : query1175px
+    ? 4
+    : stories.length;
   return (
     <div className="stories">
       <div className="create_story_card">
